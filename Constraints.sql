d69e4b67ea64e3da28e176c1c551943f52c73b60
y0001ha <yyy96110@naver.com>
2024-07-23 15:51:03 +0900

    20240723 15:50

--- Constraints.sql
+++ Constraints.sql
@@ -3,7 +3,7 @@ USE practice_sql;
 -- 제약조건 : 데이터베이스 테이블 컬럼에 삽입, 수정, 삭제 시 적용되는 규칙 
 
 -- NOT NULL 제약조건 : 해당 컬럼에 null을 포함하지 못하도록 제약 
--- (INSERT, UPDATE)에 영향을 미침
+-- (자기 자신 테이블 INSERT, UPDATE)에 영향을 미침
 
 CREATE TABLE not_null_table (
     null_column INT NULL,
@@ -25,6 +25,7 @@ SELECT * FROM not_null_table;
 UPDATE not_null_table SET not_null_column = null;
 
 -- UNIQUE 제약조건 : 해당 컬럼에 중복된 데이터를 포함할 수 없도록 하는 제약
+-- (자기 자신 테이블 INSERT, UPDATE)에 영향을 미침
 CREATE TABLE unique_table (
     unique_column INT UNIQUE,
     not_unique_column INT
@@ -40,6 +41,49 @@ SELECT * FROM unique_table;
 -- UNIQUE 로 지정된 컬럼은 중복된 데이터로 수정할 수 없음
 UPDATE unique_table SET unique_column = 3;
 UPDATE unique_table SET unique_column = 1 WHERE unique_column = 2;
+
+-- NOT NULL + UNIQUE = 후보키 
+-- 후보키 : 테이블에서 각 레코드를 고유하게 식별할 수 있는 속성들 
+-- 기본키 : 테이블에서 각 레코드를 고유하게 식별하기 위해 후보키에서 선택한 속성
+-- 대체키 : 후보키에서 기본키를 제외한 나머지 속성들
+
+-- PRIMARY KEY 제약조건 : 특정 컬럼을 기본키로 지정 
+-- (INSERT, UPDATE)에 영향을 미침
+CREATE TABLE key_table (
+	primary_column INT PRIMARY KEY,
+    surrogate_column INT NOT NULL UNIQUE
+); 
+-- PRIMARY KEY 제약조건은 NOT NULL, UNIQUE 제약조건을 모두 가지고 있음 
+INSERT INTO key_table VALUES (null, 1); -- notnull 지정 안해도 프라이머리는 안됨
+INSERT INTO key_table (surrogate_column) VALUES (1); 
+INSERT INTO key_table VALUES (1, 1);
+INSERT INTO key_table VALUES (1, 2);
+
+-- PRIMARY KEY 제약을 두 개 이상 지정 불가능
+CREATE TABLE composite_table (
+	primary1 INT PRIMARY KEY,
+	primary2 INT PRIMARY KEY
+); 
+-- 두 개 이상 걸고싶으면
+CREATE TABLE composite_table (
+	primary1 INT,
+    primary2 INT,
+    CONSTRAINT primary_key PRIMARY KEY (primary1, primary2)
+);
+
+-- FOREIGN KEY 제약조건 : 특정 컬럼을 다른 테이블 혹은 같은 테이블의 기본키 컬럼과 연결하는 제약 
+-- FOREIGN KEY 제약조건을 특정 컬럼에 적용할 때는 반드시 데이터 타입이 참조하고자 하는 컬럼의 타입과 일치해야함 
+ CREATE TABLE foreign_table (
+	primary1 INT PRIMARY KEY,
+    foreign1 INT,
+    CONSTRAINT foreign_key FOREIGN KEY (foreign1)
+    REFERENCES key_table (primary_column)
+ );  -- 여태 본 제약중에 가장 강력한 제약 
+ 
+ SELECT * FROM key_table;
+ 
+ INSERT INTO foreign_table VALUES (1, 0); 
+ INSERT INTO foreign_table VALUES (1, 1);
 
 
 
