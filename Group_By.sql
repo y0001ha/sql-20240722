138da94d80d5343ba0338354fa3ac56f4086f138
y0001ha <yyy96110@naver.com>
2024-07-25 12:43:27 +0900

    20240725 12:43

--- Group_By.sql
+++ Group_By.sql
@@ -40,7 +40,68 @@ SELECT COUNT(amount) FROM sale;
 SELECT SUM(amount) FROM sale;
 SELECT SUM(amount) FROM sale WHERE employee_number = 1;
 
+-- AVG() : 특정 조건에 해당하는 컬럼의 값의 평균을 반환 
+SELECT AVG(amount) FROM sale;
+SELECT AVG(amount) FROM sale WHERE employee_number = 1;
 
+-- MAX(), MIN() : 특정 조건에 해당하는 컬럼 값의 최대, 최소 값을 반환
+SELECT MAX(amount), MIN(amount) FROM sale;
+
+-- 그룸화 (GROUP BY) : 조회 결과에 대해 레코드를 하나 이상의 컬럼으로 그룹화하여 결과를 도출하는 것 
+-- 일반적으로 집계함수와 함께 사용됨 
+SELECT AVG(amount), employee_number
+FROM sale
+GROUP BY employee_number;
+
+SELECT AVG(amount)
+FROM sale
+GROUP BY employee_number; -- SELECT에 사용하지 않아도 됨 
+
+-- 집계함수가 포함되어 있는 쿼리 혹은 그룹화가 되어있는 쿼리에서는 
+-- 그룹화한 컬럼과 집계함수를 제외한 컬럼은 SELECT 절에서 사용이 불가능 
+SELECT AVG(amount), date
+FROM sale
+GROUP BY employee_number; 
+
+SELECT AVG(amount), employee_number, date
+FROM sale
+GROUP BY employee_number, date; -- 두개 이상 그룹화 가능 
+
+SELECT employee_number, date
+FROM sale
+GROUP BY employee_number, date; -- 집계 안해도 됨 = select distinct랑 같은 결과 
+
+-- 필터링 (HAVING) : 그룹화된 결과에 필터 작업을 수행 
+-- 주의!! WHERE 절과 사용방법이 비슷하지만 WHERE절은 조회할 때 사용이 되고, HAVING절은 조회 결과에 사용
+SELECT 
+	AVG(amount) '평균',
+	employee_number '사번'
+FROM sale
+WHERE AVG(amount) <= 110000
+GROUP BY employee_number; -- > WHERE 불가능 
+
+SELECT 
+	AVG(amount) '평균',
+	employee_number '사번'
+FROM sale
+GROUP BY employee_number
+HAVING AVG(amount) <= 110000;
+-- HAVING 평균 <= 110000; 가능
+
+SELECT 
+	AVG(amount) '평균',
+	employee_number '사번'
+FROM sale
+WHERE date < '2024-07-05'
+GROUP BY employee_number;
+
+-- 결과 테이블에 존재하지 않는 컬럼은 HAVING 절에서 사용할 수 없음
+SELECT 
+	AVG(amount) '평균',
+	employee_number '사번'
+FROM sale
+GROUP BY employee_number
+HAVING date < '2024-07-05';
 
 
 
